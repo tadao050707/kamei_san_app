4e9a947b9aa4563c19f9fa84dc5075b5b49c5e6d
sayaka kamei <sayukou316@gmail.com>
2022-11-29 17:34:11 +0900

    devise_invitable招待機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender, :age, :group_id]) 
-    # devise_parameter_sanitizer.permit(:invite, keys: [:email, :name, :gender, :age, :group_id])
-    # devise_parameter_sanitizer.permit(:accept_invitation, keys: [:password, :password_confirmation, :invitation_token, :name, :gender, :age, :group_id])
+    devise_parameter_sanitizer.permit(:invite, keys: [:email, :name, :gender, :age, :group_id])
+    devise_parameter_sanitizer.permit(:accept_invitation, keys: [:password, :password_confirmation, :invitation_token, :name, :gender, :age, :group_id])
   end
 end
---
+++ app/controllers/users/invitations_controller.rb
@@ -0,0 +1,25 @@
+class Users::InvitationsController < Devise::InvitationsController
+  def new
+    super
+  end
+
+  def create
+    super
+    @assign = Assign.new
+    @assign.user_id = User.find_by(email: params[:user][:email]).id
+    @assign.group_id = params[:user][:group_id]
+    @assign.save
+  end
+
+  def edit
+    super
+  end
+
+  def update
+    super
+  end
+
+  def destroy
+    super
+  end
+end  
