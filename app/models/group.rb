6300e38b2212aaed1b186f50592e27e6596a6b77
sayaka kamei <sayukou316@gmail.com>
2022-12-01 14:56:43 +0900

    画像アップロード機能の実装

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
   has_many :assigns, dependent: :destroy
   has_many :users, through: :assigns, source: :user
+  has_many :quizzes
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,5 @@
 class Quiz < ApplicationRecord
   belongs_to :group
   belongs_to :user
+  mount_uploader :image, ImageUploader
 end
