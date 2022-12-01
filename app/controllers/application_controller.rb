75cf1b770feb8a920be6ea64e9169598edffa11a
sayaka kamei <sayukou316@gmail.com>
2022-12-01 11:59:41 +0900

    グループ・クイズ機能の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender, :age, :group_id]) 
     devise_parameter_sanitizer.permit(:invite, keys: [:email, :name, :gender, :age, :group_id])
     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:password, :password_confirmation, :invitation_token, :name, :gender, :age, :group_id])
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
   end
 end
--- app/views/quizzes/_quiz.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! quiz, :id, :title, :image, :group_id, :user, :created_at, :updated_at
-json.url quiz_url(quiz, format: :json)
