40f0bf3fbf8ff9a14c82683554386c929d9436eb
sayaka kamei <sayukou316@gmail.com>
2022-12-12 11:37:57 +0900

    本番環境用のメール設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,18 +73,20 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'https://13.230.82.141' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                  587,
-    domain:               'smtp.gmail.com',
+    domain:               'gmail.com',
     user_name:            ENV['DB_USERNAME'], 
     password:             ENV['DB_PASSWORD'],  
     authentication:       'login',
+    openssl_verify_mode:  'none',
     enable_starttls_auto:  true
-  }  
+  }
+  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
 end
