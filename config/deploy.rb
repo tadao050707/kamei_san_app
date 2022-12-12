0224485e4ba561398f736b0b1956b638ca6748b6
sayaka kamei <sayukou316@gmail.com>
2022-12-12 18:24:44 +0900

    depioy.rbを修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,4 +64,13 @@ namespace :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
     end
   end
+
+  set :default_env, {
+    rbenv_root: "/home/user/.rbenv",
+    path: "/home/user/.rbenv/bin:$PATH",
+    region: ENV["S3_REGION"],
+    aws_access_key_id: ENV["S3_ACCESS_KEY"],
+    aws_secret_access_key: ENV["S3_SECRET_KEY"]
+  }
+  
 end
